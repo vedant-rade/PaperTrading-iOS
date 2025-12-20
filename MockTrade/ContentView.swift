b502a368e4595c03d7f7164839106ff7af34a08d
vedant-rade <vedantrade.iosdev@gmail.com>
2025-12-20 10:54:28 +0530

    Initial Commit

---
+++ MockTrade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MockTrade
+//
+//  Created by Vedant on 20/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockTrade/MockTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockTradeApp.swift
+//  MockTrade
+//
+//  Created by Vedant on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MockTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
